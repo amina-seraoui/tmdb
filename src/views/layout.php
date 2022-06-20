427a63b2a4575f68351c853f4b85ec4018ee4eb1
amina-seraoui <amina.seraoui@outlook.fr>
2022-06-20 10:07:56 +0200

    :rocket: fav icon and script

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <title>Allociné</title>
-</head>
-<body>
-    <?= $content ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="shortcut icon" href="/img/logo.svg" type="image/svg">
+        <title>Allociné</title>
+    </head>
+    <body>
+        <?= $content ?>
+        <footer>
+            <p>Copyright © 2022 - Amina Seraoui. Tous droits réservés.</p>
+        </footer>
+    </body>
 </html>
